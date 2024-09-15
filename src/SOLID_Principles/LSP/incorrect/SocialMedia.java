f08adfc9ac21b93230c94330cf0a4136f27e75ae
VAtulKumar1 <atulkumarverma304@gmail.com>
2024-09-15 17:17:41 +0530

    added a comment to LSP

--- src/SOLID_Principles/LSP/incorrect/SocialMedia.java
+++ src/SOLID_Principles/LSP/incorrect/SocialMedia.java
@@ -1,4 +1,14 @@
 package SOLID_Principles.LSP.incorrect;
+/*
+ * The Liskov Substitution Principle (LSP) states that objects of a subclass should be able to
+ * replace objects of the superclass without affecting the functionality of the program.
+ *
+ * In the example below, all methods in the base class are abstract, requiring any subclass
+ * to provide its implementation. However, if the 'SocialMedia' class is extended by a class
+ * like 'WhatsApp', the 'publishPost' method would not be relevant or useful,
+ * leading to a violation of the Liskov Substitution Principle.
+ * In this case, substituting 'SocialMedia' with 'WhatsApp' could break the expected behavior.
+ */
 
 public abstract class SocialMedia {
     public abstract void chatWithFriend();
